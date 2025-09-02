8b197f3def0bb777e8a3ecc2b8383b8d943ecb78
Maksim Vagner <maksim.vagner@noveogroup.com>
2025-09-02 16:26:35 +0700

    feat: redesign

--- src/lib/components/Container/Container.svelte
+++ src/lib/components/Container/Container.svelte
@@ -14,7 +14,7 @@
 	const { center, class: className, size = 'full', children }: Props = $props();
 
 	const styles = tv({
-		base: '',
+		base: 'bg-light dark:bg-bg',
 		variants: {
 			size: {
 				tiny: 'max-w-lg',
