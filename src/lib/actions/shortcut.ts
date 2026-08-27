8c5529cfc841223b8f1656db0fc92722a6a1ea4b
mvagner <maksim.vagner@noveogroup.com>
2026-08-27 17:12:47 +0700

    fix: slideshow input fix

--- src/lib/actions/shortcut.ts
+++ src/lib/actions/shortcut.ts
@@ -60,7 +60,9 @@ export const shouldIgnoreEvent = (event: KeyboardEvent | ClipboardEvent): boolea
     return false;
   }
   const type = (event.target as HTMLInputElement).type;
-  return ['textarea', 'text', 'date', 'datetime-local', 'email', 'password'].includes(type);
+  return ['textarea', 'text', 'number', 'date', 'datetime-local', 'email', 'password', 'search', 'tel', 'url'].includes(
+    type,
+  );
 };
 
 export const matchesShortcut = (event: KeyboardEvent, shortcut: Shortcut) => {
